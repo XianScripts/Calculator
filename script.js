cfac7fdeb05333fff6dbfa9f2762cde1d0bb17d3
Xiannomics <christfontanez@gmail.com>
2023-08-28 21:28:54 -0400

    Basic calculator functions working

---
+++ script.js
@@ -0,0 +1,28 @@
+// Calculation functions
+
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+console.log(add(5, 10));
+console.log(subtract(10, 5));
+console.log(multiply(5, 10));
+console.log(divide(5, 10));
+
+
+// operator function
+
+// if operator = x y z xA => call this function 
