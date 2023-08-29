fecd78a47da422c5728d024a2f638a1576c2b648
Christian <xian@pop-os.localdomain>
2023-08-29 08:37:11 -0400

    Working operate function

--- script.js
+++ script.js
@@ -17,14 +17,30 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
-console.log(add(5, 10));
-console.log(subtract(10, 5));
-console.log(multiply(5, 10));
-console.log(divide(5, 10));
+// console.log(add(5, 10));
+// console.log(subtract(10, 5));
+// console.log(multiply(5, 10));
+// console.log(divide(5, 10));
 
 
 // operator function
-let operator; 
-console.log(operator);
+let operator = '*'; 
+// console.log(operator);
 
+function operate(operator, num1, num2) {
+    if (operator === '+') {
+        return add(num1, num2);
+    };
+    if (operator === '-') {
+        return subtract(num1, num2);
+    };
+    if (operator === '*') {
+        return multiply(num1, num2);
+    };
+    if (operator === '/') {
+        return divide(num1, num2);
+    };
+};
+
+console.log(operate(operator, 5, 10));
 // if operator = x y z xA => call this function 
