4672e3e5c9101ed43e2719b135b9e0ff831ed098
Christian <xian@pop-os.localdomain>
2023-08-29 23:07:47 -0400

    Buttons returning className

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Calculation functions
-let num1;
-let num2;
+
 
 function add(num1, num2) {
     return num1 + num2;
@@ -18,44 +17,45 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
-console.log(add(5, 10));
-console.log(subtract(10, 5));
-console.log(multiply(5, 10));
-console.log(divide(5, 10));
+// console.log(add(5, 10));
+// console.log(subtract(10, 5));
+// console.log(multiply(5, 10));
+// console.log(divide(5, 10));
 
 
 // operator function
-let operator; 
+let operator = '/'; 
+// console.log(operator);
 
 function operate(operator, num1, num2) {
-    if (operator === "+") {
+    if (operator === '+') {
         return add(num1, num2);
-    }
-    if (operator === "-") {
+    };
+    if (operator === '-') {
         return subtract(num1, num2);
-    }
-    if (operator === "*") {
+    };
+    if (operator === '*') {
         return multiply(num1, num2);
-    }
-    if (operator === "/") {
+    };
+    if (operator === '/') {
         return divide(num1, num2);
-    }
-}
+    };
+};
 
-
-
+console.log(operate(operator, 5, 10));
 // if operator = x y z xA => call this function 
 
-// User Interface
 
 
-const buttons = document.querySelector('.buttonContainer');
 
-// function createButtons(x) {
-//     for (let i = 0; i < x; i++) {
-//         const buttonMaker = document.createElement('button');
-//         buttonMaker.classList.add('buttons');
-//         buttons.appendChild(buttonMaker);
-//     }
-// }
-// createButtons(19);
+// User interface
+//Grabbing numbers
+
+let buttonContainer = document.querySelector('.buttonContainer');
+buttonContainer.addEventListener('click', (e) => {
+    let name = e.target.classList;
+    console.log(name);
+
+
+
+});
