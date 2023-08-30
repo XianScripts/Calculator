21cc93214c42fa9238ef1147bb5ef9367e94397f
Xiannomics <christfontanez@gmail.com>
2023-08-30 16:45:23 -0400

    display reading input

--- script.js
+++ script.js
@@ -49,13 +49,54 @@ console.log(operate(operator, 5, 10));
 
 
 // User interface
+
+// Display output
+let display = document.querySelector('.displayText');
+console.log(display)
+
+
 //Grabbing numbers
 
 let buttonContainer = document.querySelector('.buttonContainer');
-buttonContainer.addEventListener('click', (e) => {
-    let name = e.target.classList;
-    console.log(name);
 
+buttonContainer.addEventListener('click', (e) => {
+    console.log(e.target.textContent);
+    let var1 = e.target.textContent;
+    display.textContent = var1;
 
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const buttonMapping = {
+//     'btn-0' : 'zero',
+//     'btn-1' : 1,
+//     'btn-2' : 2,
+//     'btn-3' : 3,
+//     'btn-4' : 4,
+//     'btn-5' : 5,
+//     'btn-6' : 6,
+//     'btn-7' : 7,
+//     'btn-8' : 8,
+//     'btn-9' : 9,
+//     'btn-AC' : 'AC',
+//     'btn-plusMinus' : 'plusMinus',
+//     'btn-mod' : 'mod',
+//     'btn-divide' : 'divide',
+//     'btn-multi' : 'multi',
+//     'btn-subtract' : 'subtract',
+//     'btn-plus' : 'plus',
+//     'btn-dot' : 'dot',
+//     'btn-equal' : 'equal',
+// }
