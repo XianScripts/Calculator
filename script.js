ca0a3fd4e6402c2daeace5772bf91341953b4ca6
Xiannomics <christfontanez@gmail.com>
2023-09-08 19:49:25 -0400

    Update

--- script.js
+++ script.js
@@ -28,73 +28,128 @@ const subtracty = '-';
 const multi = '*';
 const dividy = '/';
 
-function operate(operator, num1, num2) {
+let currentInput = '';
+let previousResult = null;
+
+function calculate() {
+
+    const num1 = previousResult !== null ? previousResult: 0;
+    const num2 = parseFloat(currentInput);
+
     if (operator === '+') {
         if (num1 === 420 && num2 === 69 || num1 === 69 && num2 === 420) {
             return ('Funny number');
         }
         return add(num1, num2);
-    };
-    if (operator === '-') {
+    }
+    else if (operator === '-') {
         return subtract(num1, num2);
-    };
-    if (operator === '*') {
+    }
+    else if (operator === '*') {
         return multiply(num1, num2);
-    };
-    if (operator === '/') {
-        if (num1 === 0 || num2 === 0) {
+    }
+    else if (operator === '/') {
+        if (num2 === 0) {
             return ('No Can Do!');
         }
         return divide(num1, num2);
-    };
+    }
+    else {
+        return num2; // If no operator return the current input as a number
+    }
 };
 
-console.log(operate(multi, 14, 52));
+// console.log(operate(multi, 14, 52));
 
 
 //Grabbing numbers
-let num1;
-let num2;
-let storedNumber = '';
+// let num1;
+// let num2;
+// let storedNumber = '';
 // let calComplete = false;
 // let percentActive = false;
 // let plusMinus = false;
 // const maxLength = 10;
 
-let buttons = document.querySelectorAll('.btn');
-console.log(buttons);
+// let buttons = document.querySelectorAll('.btn');
+// console.log(buttons);
 // User interface
 
 // Display output
 let displayOutput = document.querySelector('.displayText');
+
 // displayOutput.textContent = "Testing";
-function numberDisplay() {
-    buttons.forEach(button => {
-        button.addEventListener('click', (e) => {
-            const key = e.target.textContent;
-            
-            if ( !((key === addy) || (key === subtracty) || (key === multi) || (key === dividy)) ) {
-                displayOutput.textContent += key;
 
-                console.log(key);
-            }
-        })
-    })
-};
-
-function operateDisplay() {
-    buttons.forEach(button => {
-        button.addEventListener('click', (e) => {
-            const key = e.target.textContent;
-            if ( (key === addy) || (key === subtracty) || (key === multi) || (key === dividy) ) {
-                console.log('Operaton key: ' + key);
-            }
-        })
-    })
+function updateDisplay() {
+    displayOutput.textContent = currentInput;
+    if (displayOutput.textContent.length > 10 ) {
+        displayOutput.textContent = displayOutput.textContent.slice(0, 12);
+    }
 }
 
-numberDisplay();
-operateDisplay();
+
+
+document.querySelectorAll('.btn').forEach(button => {
+    button.addEventListener('click', (e) => {
+        const key = e.target.textContent;
+
+        if (key === '=') {
+            if (operator && currentInput !== '') {
+                previousResult = calculate();
+                currentInput = previousResult.toString();
+                operator = null;
+                updateDisplay();
+            }
+        }
+        else if (key === 'AC') {
+            currentInput = '';
+            previousResult = null;
+            operator = null;
+            updateDisplay();
+        }
+        else {
+            if (key.match(/[0-9.]/)) {
+                currentInput += key; // Append numbers and decimal point
+                updateDisplay();
+            } 
+            else if (key === '+') {
+                operator = key;
+                previousResult = parseFloat(currentInput);
+                currentInput = '';
+            } 
+            else if (key === '-') {
+                operator = key;
+                previousResult = parseFloat(currentInput);
+                currentInput = '';
+            } 
+            else if (key === '*') {
+                operator = key;
+                previousResult = parseFloat(currentInput);
+                currentInput = '';
+            } 
+            else if (key === '/') {
+                operator = key;
+                previousResult = parseFloat(currentInput);
+                currentInput = '';
+            }
+            else if (key === '+/-') {
+                currentInput = currentInput * -1;
+                updateDisplay();
+            }
+            else if (key === '%') {
+                currentInput = currentInput / 100;
+                updateDisplay();
+            }
+        }
+    });
+});
+
+
+
+
+updateDisplay();
+
+
 
 
 
